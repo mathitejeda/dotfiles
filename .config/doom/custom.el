47f13cf02713ae6e9197a150034a7247afc88c37
mathitejeda <mathi.tejeda@gmail.com>
2024-07-22 16:16:19 -0300

    Agregue plugin bullets para orgmode. Cambios en config tmux

---
+++ .config/doom/custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8b130d9b896ba883fc99ebfbf1bba5600e228793bd08eebd89e239daecd9d7af" "6431197c878808fafc11ee1278bf21b709537d1fb377bc32bfcf7a5cfbeb3d63" "f5a7c10fbb726e90a3befca828b2a4de493404a13ce42dcbeb5212ecd553189e" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
+ '(custom-theme-directory "/Users/mathias/.config/doom/themes/")
+ '(display-line-numbers-widen t)
+ '(display-line-numbers-width 3)
+ '(doom-rose-pine-brighter-modeline t)
+ '(lsp-perlnavigator-include-paths
+   ["/epl/src/perl/perl" "/epl/src/admin/perl" "/epl/src/devel/perl" "/epl/src/adserver/tests" "/epl/src/admin/tests/admin" "/epl/src/admin/tests/api" "/epl/src/adserver/tests" "/epl/src/formatos/tests"])
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files '("~/org/" "~/notes/trabajo"))
+ '(package-selected-packages '(ripgrep nerd-icons rg perl-doc))
+ '(vterm-shell "/bin/fish")
+ '(vterm-tramp-shells '(("docker" "/bin/bash"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#232136" :foreground "#e0def4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Menlo"))))
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
+(put 'projectile-ripgrep 'disabled nil)
--- .config/nvim/lua/mathi/init.lua
+++ .config/nvim/lua/mathi/init.lua
@@ -1,3 +1,7 @@
 require("mathi.remap")
 require("mathi.init_lazy")
 require("mathi.set")
+
+-- Para links de orgmode
+vim.opt.conceallevel = 2
+vim.opt.concealcursor = 'nc'
