beef58c50606fa68348aab15c784116b542e5e19
mathitejeda <mathi.tejeda@gmail.com>
2024-08-22 15:06:42 -0300

    se agregan dashboard lualine y wichkey. El ultimo sin modificar

---
+++ .config/nvim/lua/mathi/lazy/wichkey.lua
@@ -0,0 +1,18 @@
+return {
+-- Terminar de configurar, tengo que mover las remaps a este archivo y configurar para que suba el statusline y que su subida sea mas amena
+--   {
+--      "folke/which-key.nvim",
+--      event = "VeryLazy",
+--      opts = {
+--      },
+--      keys = {
+--        {
+--          "<leader> ",
+--          function()
+--            require("which-key").show({ global = false })
+--          end,
+--          desc = "Buffer Local Keymaps (which-key)",
+--        },
+--      },
+--    }
+}
