c7ca1dc1d59684fc192e5f00c4ce87c96e16582d
mathitejeda <mathi.tejeda@gmail.com>
2024-09-02 23:20:08 -0300

    Al final lo activo igual y lo voy configurando sobre la marcha

--- .config/nvim/lua/mathi/lazy/wichkey.lua
+++ .config/nvim/lua/mathi/lazy/wichkey.lua
@@ -1,24 +1,24 @@
 return {
     -- Terminar de configurar, tengo que mover las remaps a este archivo y configurar para que suba el statusline y que su subida sea mas amena
---    {
---        "folke/which-key.nvim",
---        event = "VeryLazy",
---        opts = {
---        },
---        keys = {
---            {
---                "<leader> ",
---                function()
---                    require("which-key").show({ global = false })
---                end,
---                desc = "Buffer Local Keymaps (which-key)",
---            },
---        },
---        config = function()
---            require("which-key").add({
---                { "<leader>f", group = "File" },
---                { "<leader>b", group = "Buffer" },
---            })
---        end
---    }
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+        },
+        keys = {
+            {
+                "<leader> ",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+        config = function()
+            require("which-key").add({
+                { "<leader>f", group = "File" },
+                { "<leader>b", group = "Buffer" },
+            })
+        end
+    }
 }
