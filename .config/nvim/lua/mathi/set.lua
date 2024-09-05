c06abdbee5709962615279babd296f1a3b19155d
mathitejeda <mathi.tejeda@gmail.com>
2024-09-05 16:25:21 -0300

    Modificacion de tabs para que sean realmente tabs

--- .config/nvim/lua/mathi/set.lua
+++ .config/nvim/lua/mathi/set.lua
@@ -4,11 +4,8 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
+vim.opt.expandtab = false
 
 vim.opt.wrap = true
 
@@ -33,3 +30,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.list = true
+
+vim.opt.concealcursor = 'nc'
+vim.opt.conceallevel = 2
