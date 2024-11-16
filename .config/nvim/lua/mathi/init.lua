471c097a1394016214870edc535cfd88d8eba7fe
mathitejeda <mathi.tejeda@gmail.com>
2024-11-16 18:32:10 -0300

    agregue tema doom one para kitty y nvim

--- .config/nvim/lua/mathi/init.lua
+++ .config/nvim/lua/mathi/init.lua
@@ -2,6 +2,8 @@ require("mathi.remap")
 require("mathi.set")
 require("mathi.init_lazy")
 
+ColorMyPencils("doom-one")
+
 -- Para links de orgmode
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = 'nc'
