8c6fca548c5e4dddbef981c26c3a48768b557770
mathitejeda <mathi.tejeda@gmail.com>
2024-12-01 15:47:27 -0300

    borre comentarios de ufo y agregue una configuracion para que cuando abro un archivo org no se pelotudean los foldings. configuraciones de orgmode

--- .config/nvim/lua/mathi/lazy/orgmode/nvim-orgmode.lua
+++ .config/nvim/lua/mathi/lazy/orgmode/nvim-orgmode.lua
@@ -6,7 +6,8 @@ return {
     -- Setup orgmode
     require('orgmode').setup({
       org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_default_notes_file = '~/notes/index.org',
+	  org_startup_folded = 'content',
     })
   end,
 }
