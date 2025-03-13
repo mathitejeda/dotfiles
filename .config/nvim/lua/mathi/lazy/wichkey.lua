ac14eeb65dde5aa4cc7db198b78bad1bc69c5717
mathitejeda <mathi.tejeda@gmail.com>
2025-03-13 14:26:17 -0300

    overlay git y format

--- .config/nvim/lua/mathi/lazy/wichkey.lua
+++ .config/nvim/lua/mathi/lazy/wichkey.lua
@@ -1,24 +1,26 @@
 return {
-    -- Terminar de configurar, tengo que mover las remaps a este archivo y configurar para que suba el statusline y que su subida sea mas amena
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-        },
-        keys = {
-            {
-                "<leader> ",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-        config = function()
-            require("which-key").add({
-                { "<leader>f", group = "File" },
-                { "<leader>b", group = "Buffer" },
-            })
-        end
-    }
+	-- Terminar de configurar, tengo que mover las remaps a este archivo y configurar para que suba el statusline y que su subida sea mas amena
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+		},
+		keys = {
+			{
+				"<leader> ",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+		config = function()
+			require("which-key").add({
+				{ "<leader>f", group = "File" },
+				{ "<leader>b", group = "Buffer" },
+				{ "<leader>g", group = "Git" },
+				{ "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<cr>", desc = "Git toggle overlay" },
+			})
+		end
+	}
 }
