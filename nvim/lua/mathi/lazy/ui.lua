c1138a0f949a4f204f2dc5296c48771ade9672f7
mathitejeda <mathi.tejeda@gmail.com>
2025-04-09 10:57:13 -0300

    ui

--- nvim/lua/mathi/lazy/ui.lua
+++ nvim/lua/mathi/lazy/ui.lua
@@ -1,11 +1,11 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    input = { enabled = true },
-    notifier = { enabled = true },
-    scroll = { enabled = true },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		input = { enabled = true },
+		notifier = { enabled = true },
+		scroll = { enabled = true },
+	},
 }
